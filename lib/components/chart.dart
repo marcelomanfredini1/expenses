b977fb00403cc68263cf7cc1ec181558255a9706
marcelo manfredini <marcelomanfredini1@gmail.com>
2023-08-05 21:50:11 -0300

    Initial commit

---
+++ lib/components/chart.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+import 'chart_bar.dart';
+
+class Chart extends StatelessWidget {
+  //const Chart({Key? key}) : super(key: key);
+
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        final tr = recentTransactions[i];
+
+        if (tr.date.day == weekDay.day &&
+            tr.date.month == weekDay.month &&
+            tr.date.year == weekDay.year) {
+          totalSum += tr.value;
+        }
+      }
+
+      /*print(DateFormat.E().format(weekDay)[0]);
+      print(totalSum);*/
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    }).reversed.toList();
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'].toString(),
+                value: tr['value'] as double,
+                percentage: _weekTotalValue == 0
+                    ? 0
+                    : (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  //const ChartBar({Key? key}) : super(key: key);
+
+  final String label;
+  final double value;
+  final double percentage;
+
+  const ChartBar({
+    required this.label,
+    required this.value,
+    required this.percentage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('${value.toStringAsFixed(2)}'),
+          ),
+        ),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 1,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label),
+      ],
+    );
+  }
+}
+
+
+/*  */
+
