c0915a63ba2a639ab247e7e7604d5d9081543dc3
marcelo manfredini <marcelomanfredini1@gmail.com>
2023-08-09 18:20:36 -0300

    Finalização do App Despesas

--- lib/main.dart
+++ lib/main.dart
@@ -92,8 +92,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandScape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandScape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: const Text('Despesas Pessoais'),
@@ -114,9 +114,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availablelHeight = MediaQuery.of(context).size.height -
+    final availablelHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
